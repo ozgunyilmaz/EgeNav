2092a8404d5b1b491e2a2a1a220c2ca713c7feb4
ozgunyilmaz35@gmail.com <ozgunyilmaz35@gmail.com@8d07ed20-8728-801f-ce08-516e7f655ef3>
2014-04-14 09:46:21 +0000

    Initial upload of the project

---
+++ trunk/EgeNav/src/egenav/cs/ege/edu/tr/Coordinate.java
@@ -0,0 +1,107 @@
+package egenav.cs.ege.edu.tr;
+
+/**
+ * @author Özgün Yılmaz
+ * Created on 04.Nis.2014, 13:44:59
+ */
+//Koordinat sınıfı
+//enlem ve boylam değereleri String'den double'a çevriliyor.
+
+public class Coordinate{
+	
+    private double lat,lon;	//enlem ve boylam
+
+    public Coordinate(double lat,double lon){
+        this.lat=lat;
+        this.lon=lon;
+    }
+
+    public Coordinate(String p){
+        //Parametre olarak enlem ve boylam'dan oluşan string alınır.
+        double dec,min,sec;
+        //Aşağıda enlem için derece, dakika, saniye değerleri bulunuyor.		
+        int i=0;
+        while (Character.isDigit(p.charAt(i))){
+                i++;
+        }
+
+        dec=Integer.parseInt(p.substring(0,i));
+        i++;
+
+        int ii=i;
+        while (Character.isDigit(p.charAt(i))){
+                i++;
+        }
+        min=Integer.parseInt(p.substring(ii,i));
+        i++;
+
+        ii=i;
+        while (Character.isDigit(p.charAt(i))){
+                i++;
+        }
+        sec=Integer.parseInt(p.substring(ii,i));
+
+
+        lat=dec+min/60+sec/3600;
+        //enlem değeri hesaplanıyor.
+
+        if (p.charAt(i)=='s' || p.charAt(i)=='S'){
+                lat=lat*(-1);	//Eğer enlem güneyse(South-S) işareti -'dir.
+        }
+
+
+        //Aşağıda boylam için derece, dakika, saniye değerleri bulunuyor.	
+        i=i+2;
+        ii=i;
+        while (Character.isDigit(p.charAt(i))){
+                i++;
+        }
+        dec=Integer.parseInt(p.substring(ii,i));
+        i++;
+
+        ii=i;
+        while (Character.isDigit(p.charAt(i))){
+                i++;
+        }
+        min=Integer.parseInt(p.substring(ii,i));
+        i++;
+
+        ii=i;
+        while (Character.isDigit(p.charAt(i))){
+                i++;
+        }
+        sec=Integer.parseInt(p.substring(ii,i));
+
+
+        lon=dec+min/60+sec/3600;
+
+        if (p.charAt(i)=='w' || p.charAt(i)=='W'){
+                lon=lon*(-1);	//Eğer boylam batıysa(West-W) işareti - olmalıdır.
+        }
+
+
+    }
+
+    @Override
+    public String toString(){
+        return lat+","+lon;
+    }
+
+    //Aşağıda getter ve setter metodları bulunmaktadır.
+    public double getLatitude(){
+        return lat;
+    }
+
+    public double getLongitude(){
+        return lon;
+    }
+
+    public void setLatitude(double lat){
+        this.lat=lat;
+    }
+
+    public void setLongitude(double lon){
+        this.lon=lon;
+    }
+	
+}
---
+++ trunk/EgeNav/src/egenav/cs/ege/edu/tr/Test.java
@@ -0,0 +1,19 @@
+package egenav.cs.ege.edu.tr;
+
+/**
+ * @author Özgün Yılmaz
+ * Created on 04.Nis.2014, 15:17:44
+ */
+public class Test {
+    
+     public static void main(String args[]) {
+         GSMMapURLBuilder url=new GSMMapURLBuilder();
+         
+         url.setCenter(new MapCenter(new Coordinate(40,30)));
+         url.setZoom(7);
+         url.setMapSize(new MapSize(500,500));
+         url.setMapType(GSMMapType.HYBRID);
+         url.setFormat(GSMImageFormat.PNG);
+         System.out.println(url);
+     }
+}
