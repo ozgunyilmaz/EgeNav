89fe86185ac696e2234c77fbf3f55f57234bbdf0
ozgunyilmaz35@gmail.com <ozgunyilmaz35@gmail.com@8d07ed20-8728-801f-ce08-516e7f655ef3>
2014-04-14 09:52:22 +0000

    Initial upload of the project

--- trunk/EgeNav/src/egenav/cs/ege/edu/tr/GSMStyleRule.java
+++
@@ -1,9 +0,0 @@
-package egenav.cs.ege.edu.tr;
-
-/**
- * @author Özgün Yılmaz
- * Created on 07.Nis.2014, 16:20:48
- */
-public class GSMStyleRule {
-    //toString yaz + feature ve element sabitlerinin olduğu sınıflar
-}
