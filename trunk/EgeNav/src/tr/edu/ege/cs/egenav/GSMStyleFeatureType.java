d8c51f205e73a510d6b6226beb1482d9921140c0
ozgunyilmaz35@gmail.com <ozgunyilmaz35@gmail.com@8d07ed20-8728-801f-ce08-516e7f655ef3>
2014-04-14 10:47:37 +0000

    Style, element type constants

---
+++ trunk/EgeNav/src/tr/edu/ege/cs/egenav/GSMStyleFeatureType.java
@@ -0,0 +1,43 @@
+package tr.edu.ege.cs.egenav;
+
+/**
+ * @author Özgün Yılmaz
+ * Created on 14.Nis.2014, 13:22:52
+ */
+public class GSMStyleFeatureType {
+    
+    public static final String ADMINISTRATIVE="administrative";
+    public static final String ADMINISTRATIVE_COUNTRY="administrative.country";
+    public static final String ADMINISTRATIVE_LAND_PARCEL="administrative.land_parcel";
+    public static final String ADMINISTRATIVE_LOCALITY="administrative.locality";
+    public static final String ADMINISTRATIVE_NEIGHBORHOOD="administrative.neighborhood";
+    public static final String ADMINISTRATIVE_PROVINCE="administrative.province";
+    public static final String ALL="all";
+    public static final String LANDSCAPE="landscape";
+    public static final String LANDSCAPE_MAN_MADE="landscape.man_made";
+    public static final String LANDSCAPE_NATURAL="landscape.natural";
+    public static final String LANDSCAPE_NATURAL_LANDCOVER="landscape.natural.landcover";
+    public static final String LANDSCAPE_NATURAL_TERRAIN="landscape.natural.terrain";
+    public static final String POI="poi";
+    public static final String POI_ATTRACTION="poi.attraction";
+    public static final String POI_BUSINESS="poi.business";
+    public static final String POI_GOVERNMENT="poi.government";
+    public static final String POI_MEDICAL="poi.medical";
+    public static final String POI_PARK="poi.park";
+    public static final String POI_PLACE_OF_WORSHIP="poi.place_of_worship";
+    public static final String POI_SCHOOL="poi.school";
+    public static final String POI_SPORTS_COMPLEX="poi.sports_complex";
+    public static final String ROAD="road";
+    public static final String ROAD_ARTERIAL="road.arterial";
+    public static final String ROAD_HIGHWAY="road.highway";
+    public static final String ROAD_HIGHWAY_CONTROLLED_ACCESS="road.highway.controlled_access";
+    public static final String ROAD_LOCAL="road.local";
+    public static final String TRANSIT="transit";
+    public static final String TRANSIT_LINE="transit.line";
+    public static final String TRANSIT_STATION="transit.station";
+    public static final String TRANSIT_STATION_AIRPORT="transit.station.airport";
+    public static final String TRANSIT_STATION_BUS="transit.station.bus";
+    public static final String TRANSIT_STATION_RAIL="transit.station.rail";
+    public static final String WATER="water";
+    
+}
--- trunk/EgeNav/src/tr/edu/ege/cs/egenav/GSMStyleRule.java
+++ trunk/EgeNav/src/tr/edu/ege/cs/egenav/GSMStyleRule.java
@@ -5,5 +5,5 @@ package tr.edu.ege.cs.egenav;
  * Created on 07.Nis.2014, 16:20:48
  */
 public class GSMStyleRule {
-    //toString yaz + feature ve element sabitlerinin olduğu sınıflar
+    //toString yaz + feature ve element sabitlerinin olduğu sınıfları yaz
 }
