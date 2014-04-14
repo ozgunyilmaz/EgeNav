bacc003ab6f4946811c4d2f406ba67387a6b2232
ozgunyilmaz35 <ozgunyilmaz35@gmail.com>
2014-04-14 13:25:33 +0000

    Style, element type constants

--- EgeNav/src/tr/edu/ege/cs/egenav/GSMStyleElementType.java
+++ EgeNav/src/tr/edu/ege/cs/egenav/GSMStyleElementType.java
@@ -6,4 +6,15 @@ package tr.edu.ege.cs.egenav;
  */
 public class GSMStyleElementType {
     
+    public static final String ALL="all";
+    public static final String GEOMETRY="geometry";
+    public static final String GEOMETRY_FILL="geometry.fill";
+    public static final String GEOMETRY_STROKE="geometry.stroke";
+    public static final String LABELS="labels";
+    public static final String LABELS_ICON="labels.icon";
+    public static final String LABELS_TEXT="labels.text";
+    public static final String LABELS_TEXT_FILL="labels.text.fill";
+    public static final String LABELS_TEXT_STROKE="labels.text.stroke";
+
+    
 }
