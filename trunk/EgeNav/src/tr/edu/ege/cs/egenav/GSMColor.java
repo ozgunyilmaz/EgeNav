850b8951ef38a0ded567c5439401ec6c7258909b
ozgunyilmaz35@gmail.com <ozgunyilmaz35@gmail.com@8d07ed20-8728-801f-ce08-516e7f655ef3>
2014-04-16 08:41:07 +0000

    GSMColor added: Color class for google maps

---
+++ trunk/EgeNav/src/tr/edu/ege/cs/egenav/GSMColor.java
@@ -0,0 +1,22 @@
+package tr.edu.ege.cs.egenav;
+
+/**
+ * @author Özgün Yılmaz
+ * Created on 16.Nis.2014, 11:37:12
+ */
+public class GSMColor {
+    
+    public static final String BLACK="black";
+    public static final String BROWN="brown";
+    public static final String GREEN="green";
+    public static final String PURPLE="purple";
+    public static final String YELLOW="yellow";
+    public static final String BLUE="blue";
+    public static final String GRAY="gray";
+    public static final String ORANGE="orange";
+    public static final String RED="red";
+    public static final String WHITE="white";
+    
+    
+    
+}
