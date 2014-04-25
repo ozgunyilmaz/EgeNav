8edde7790c7b24a0bf95d6048d2dc28279d50b47
ozgunyilmaz35@gmail.com <ozgunyilmaz35@gmail.com@8d07ed20-8728-801f-ce08-516e7f655ef3>
2014-04-25 13:01:02 +0000

    implementing continued: plot coordinates on the image

--- trunk/EgeNav/src/tr/edu/ege/cs/egenav/ui/MapPanel.java
+++ trunk/EgeNav/src/tr/edu/ege/cs/egenav/ui/MapPanel.java
@@ -9,10 +9,12 @@ import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 import tr.edu.ege.cs.egenav.Location;
 import tr.edu.ege.cs.egenav.MapDownloader;
 import tr.edu.ege.cs.egenav.MapURL;
@@ -28,7 +30,7 @@ public class MapPanel extends javax.swing.JPanel {
     private InputStream in;
     private double x1,y1,x2,y2;
     private boolean enforceCenter=false;
-    private ArrayList<Location> history=new ArrayList<Location>();
+    private ArrayList<NavigationInfo> history=new ArrayList<NavigationInfo>();
     private int heading=-1,speed=-1;
     private Arrow arrow=new Arrow();
     private LineStyle routeLineStyle=new LineStyle();
@@ -120,7 +122,16 @@ public class MapPanel extends javax.swing.JPanel {
     public void updateLocation(Location loc){
         //todo navigasyon sırasında time stampleri ve noktadan noktaya uzaklık hesaplanmalı
         //todo navigationinfo sınıfını kullan.
-        history.add(loc);
+        long timestamp=Calendar.getInstance().getTimeInMillis();
+        double distance;
+        if (history.isEmpty()){
+            distance=0;
+        }else{
+            NavigationInfo ni=history.get(history.size()-1);
+            distance=ni.getLocation().getDistanceTo(loc);
+        }
+        //todo pikselleri de hesaplayıp navinfonun içine koy
+        history.add(new NavigationInfo(loc,timestamp,distance));
         if (enforceCenter){
             mapurl.setLocation(loc.clone());
         }else{
@@ -146,23 +157,72 @@ public class MapPanel extends javax.swing.JPanel {
         Graphics2D g2d=(Graphics2D)g;
         g2d.drawImage(img, 0, 0, null);
         //**************************draw route
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setColor(getRouteLineStyle().getColor());
-        if (getRouteLineStyle().getStroke()!=null){
-            g2d.setStroke(getRouteLineStyle().getStroke());
-        }
-        g2d.setComposite(getRouteLineStyle().getComposite());
-        //...
         
-        //*****************************
-        
-        //***************************draw arrow
-        g2d.setColor(getArrow().getLineStyle().getColor());
-        if (getArrow().getLineStyle().getStroke()!=null){
-            g2d.setStroke(getArrow().getLineStyle().getStroke());
+        if (history.size()>1){
+            
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setColor(getRouteLineStyle().getColor());
+            if (getRouteLineStyle().getStroke()!=null){
+                g2d.setStroke(getRouteLineStyle().getStroke());
+            }
+            g2d.setComposite(getRouteLineStyle().getComposite());
+            //...
+            NavigationInfo ng1,ng2;
+            Point p1=null,p2=null;
+
+            for (int i=0;i<history.size()-1;i++){
+
+                ng1=history.get(i);
+                ng2=history.get(i+1);
+                p1=mapurl.getPixelOnMap(ng1.getLocation().getLatitude(),ng1.getLocation().getLongitude());
+                p2=mapurl.getPixelOnMap(ng2.getLocation().getLatitude(),ng2.getLocation().getLongitude());
+                g2d.drawLine((int)p1.getX(),(int)p1.getY(),(int)p2.getX(),(int)p2.getY());
+
+            }
+
+            //*****************************
+
+            //***************************draw arrow
+            g2d.setColor(getArrow().getLineStyle().getColor());
+            if (getArrow().getLineStyle().getStroke()!=null){
+                g2d.setStroke(getArrow().getLineStyle().getStroke());
+            }
+            g2d.setComposite(getArrow().getLineStyle().getComposite());
+            //...
+            int x2=(int)p2.getX(),y2=(int)p2.getY();
+
+            g2d.translate(x2, y2);
+            
+            g2d.rotate(calcArrowDegree(p1,p2)+Math.PI/2);
+
+            g2d.fill(getArrow().getShape());
         }
-        g2d.setComposite(getArrow().getLineStyle().getComposite());
-        //...
+        
+    }
+    
+    private double calcArrowDegree(Point p1, Point p2){
+        int x1=(int)p1.getX(),y1=(int)p1.getY(),x2=(int)p2.getX(),y2=(int)p2.getY();
+        double degree;
+
+
+            if (x1==x2){
+                degree=Math.toRadians(90);
+                if (y2>y1){
+                    degree=Math.toRadians(90);
+                    //System.out.print(degree);
+                }else{
+                    degree=Math.toRadians(90)+Math.toRadians(180);
+                }
+            }else{
+                if (x2>x1){
+                    degree=Math.atan((y2-y1)/(x2-x1));
+                    System.out.print(degree);
+                }else{
+                    degree=Math.atan((y2-y1)/(x2-x1))+Math.toRadians(180);
+                }
+
+            }
+            return degree;
     }
     
     @Override
