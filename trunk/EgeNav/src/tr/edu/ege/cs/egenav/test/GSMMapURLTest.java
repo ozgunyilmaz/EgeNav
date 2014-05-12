d069657b8a140d9fb2a16dc0dba7afb18d14cb78
ozgunyilmaz35@gmail.com <ozgunyilmaz35@gmail.com@8d07ed20-8728-801f-ce08-516e7f655ef3>
2014-05-12 07:01:58 +0000

    refactoring

--- trunk/EgeNav/src/tr/edu/ege/cs/egenav/test/GSMMapURLTest.java
+++ trunk/EgeNav/src/tr/edu/ege/cs/egenav/test/GSMMapURLTest.java
@@ -3,8 +3,6 @@ package tr.edu.ege.cs.egenav.test;
 import java.awt.Point;
 import tr.edu.ege.cs.egenav.Direction;
 import tr.edu.ege.cs.egenav.GeoPoint;
-import tr.edu.ege.cs.egenav.GSMImageFormat;
-import tr.edu.ege.cs.egenav.GSMMapType;
 import tr.edu.ege.cs.egenav.GSMMapURL;
 import tr.edu.ege.cs.egenav.GSMLocation;
 import tr.edu.ege.cs.egenav.MapSize;
