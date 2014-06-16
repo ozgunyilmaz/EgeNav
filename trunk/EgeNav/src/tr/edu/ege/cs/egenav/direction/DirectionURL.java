3b7868634c870bbe926649cb1b87f59d7f0c0ffe
ozgunyilmaz35@gmail.com <ozgunyilmaz35@gmail.com@8d07ed20-8728-801f-ce08-516e7f655ef3>
2014-06-16 11:29:31 +0000

    minor refactoring

--- trunk/EgeNav/src/tr/edu/ege/cs/egenav/direction/DirectionURL.java
+++ trunk/EgeNav/src/tr/edu/ege/cs/egenav/direction/DirectionURL.java
@@ -2,7 +2,6 @@ package tr.edu.ege.cs.egenav.direction;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import tr.edu.ege.cs.egenav.GeoPoint;
 import tr.edu.ege.cs.egenav.Location;
 import tr.edu.ege.cs.egenav.Parameter;
 
@@ -103,40 +102,6 @@ public abstract class DirectionURL {
 
     public void setClientID(String clientID) {
         this.clientID = clientID;
-    }
-    
-    public static ArrayList<GeoPoint> decodePolyline(String encoded) {
-
-	ArrayList<GeoPoint> poly = new ArrayList<GeoPoint>();
-	int index = 0, len = encoded.length();
-	int lat = 0, lon = 0;
-
-	while (index < len) {
-		int b, shift = 0, result = 0;
-		do {
-			b = encoded.charAt(index++) - 63;
-			result |= (b & 0x1f) << shift;
-			shift += 5;
-		} while (b >= 0x20);
-		int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-		lat += dlat;
-
-		shift = 0;
-		result = 0;
-		do {
-			b = encoded.charAt(index++) - 63;
-			result |= (b & 0x1f) << shift;
-			shift += 5;
-		} while (b >= 0x20);
-		int dlon = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-		lon += dlon;
-
-		GeoPoint p = new GeoPoint((int) (((double) lat / 1E5) * 1E6),
-			 (int) (((double) lon / 1E5) * 1E6));
-		poly.add(p);
-	}
-
-	return poly;
     }
     
 }
