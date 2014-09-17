db23e26a2ed86860dd619c708874515bbf4a7673
ozgunyilmaz35@gmail.com <ozgunyilmaz35@gmail.com@8d07ed20-8728-801f-ce08-516e7f655ef3>
2014-09-17 13:13:04 +0000

    some small changes

--- trunk/EgeNav/src/tr/edu/ege/cs/egenav/MapPosition.java
+++ trunk/EgeNav/src/tr/edu/ege/cs/egenav/MapPosition.java
@@ -1,5 +1,7 @@
 package tr.edu.ege.cs.egenav;
 
+import tr.edu.ege.cs.egenav.direction.Directions;
+
 /**
  * @author Özgün Yılmaz
  * Created on 08.Eyl.2014, 16:33:14
@@ -8,6 +10,7 @@ public class MapPosition {
     
     private boolean intersection;
     private int vertical,horizontal;
+    private int deltaVertical,deltaHorizontal;
 
     public MapPosition(boolean intersection, int vertical, int horizontal) {
         this.intersection = intersection;
@@ -19,11 +22,11 @@ public class MapPosition {
         this.intersection = intersection;
     }
     
-    public int getHorizontal() {
+    public int getHorizontalDirection() {
         return horizontal;
     }
 
-    public void setHorizontal(int horizontal) {
+    public void setHorizontalDirection(int horizontal) {
         this.horizontal = horizontal;
     }
 
@@ -35,12 +38,44 @@ public class MapPosition {
         this.intersection = intersection;
     }
 
-    public int getVertical() {
+    public int getVerticalDirection() {
         return vertical;
     }
 
-    public void setVertical(int vertical) {
+    public void setVerticalDirection(int vertical) {
         this.vertical = vertical;
+    }
+
+    public int getDeltaHorizontal() {
+        return deltaHorizontal;
+    }
+
+    public void setDeltaHorizontal(int deltaHorizontal) {
+        this.deltaHorizontal = deltaHorizontal;
+        if (deltaHorizontal>0){
+            setHorizontalDirection(Directions.EAST);
+        }else if (deltaHorizontal<0){
+            setHorizontalDirection(Directions.WEST);
+        }else{
+            setHorizontalDirection(Directions.CONSTANT);
+        }
+        
+    }
+
+    public int getDeltaVertical() {
+        return deltaVertical;
+    }
+
+    public void setDeltaVertical(int deltaVertical) {
+        this.deltaVertical = deltaVertical;
+        
+        if (deltaVertical>0){
+            setVerticalDirection(Directions.SOUTH);
+        }else if (deltaVertical<0){
+            setVerticalDirection(Directions.NORTH);
+        }else{
+            setVerticalDirection(Directions.CONSTANT);
+        }
     }
     
 }
